cadb2a9b97a00ca768f1bf3bcb7e089d3842b4ad
DaanV2 <daanverstraten@hotmail.com>
2021-08-14 22:43:34 +0200

    Updated block tests

--- test/General/Block.test.ts
+++ test/General/Block.test.ts
@@ -21,13 +21,13 @@ describe("Block", () => {
   it("get states", () => {
     const states = Block.getStates(blockDescription);
 
-    expect(states).to.have.members(blockStates);
+    expect(states).to.have.deep.members(blockStates, "Block states do not match");
   });
 
   it("parse", () => {
     const block = Block.fromBlockDescriptor(blockDescription, Location.create("", 0));
 
     expect(block.id).to.equal(blockID);
-    expect(block.states).to.have.members(blockStates);
+    expect(block.states).to.have.deep.members(blockStates, "Block states do not match");
   });
 });
