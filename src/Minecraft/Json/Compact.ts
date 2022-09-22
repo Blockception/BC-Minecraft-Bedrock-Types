9ab6e126b3ddfcda753925cae33ae1497cad0c15
DaanV2 <daanverstraten@hotmail.com>
2022-09-22 20:24:17 +0200

    Fixed

--- src/Minecraft/Json/Compact.ts
+++ src/Minecraft/Json/Compact.ts
@@ -171,6 +171,9 @@ export namespace CompactJson {
     if (hasKey(node)) {
       offset += node.key.length + 1;
     }
+    if (node.negative) {
+      offset ++;
+    }
 
     return {
       offset: offset,
