b94c9ce40979de7a50aa99e2a3616cad9ae49e3d
Xterionix <xterionyt@gmail.com>
2025-04-24 10:37:00 +0500

    - Util functions for format versions

--- src/minecraft/format-version.ts
+++ src/minecraft/format-version.ts
@@ -21,4 +21,16 @@ export namespace FormatVersion {
 
     return [major, minor, patch];
   }
+
+  export function isEqual(a: [number, number, number], b: [number, number, number]) {
+    return a[0] === b[0] && a[1] === b[1] && a[2] === b[2];
+  }
+
+  export function isLessThan(a: [number, number, number], b: [number, number, number]) {
+    for (let i = 0; i < 3; i++) {
+      if (a[i] < b[i]) return true;
+      if (a[i] > b[i]) return false;
+    }
+    return false;
+  }
 }
